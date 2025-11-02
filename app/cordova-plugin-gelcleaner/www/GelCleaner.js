c126a4b1b44142cbf00b402f7f8429d0e7d7421e
GDiolitsis Engine Lab <support@gdiolitsis.dev>
2025-11-02 13:29:08 +0000

    Restore cordova-plugin-gelcleaner folder

---
+++ app/cordova-plugin-gelcleaner/www/GelCleaner.js
@@ -0,0 +1,24 @@
+var exec = require('cordova/exec');
+
+var GELCleaner = {
+  clearAppCache: function (success, error) {
+    exec(success, error, 'GELCleaner', 'clearAppCache', []);
+  },
+  boostRAM: function (success, error) {
+    exec(success, error, 'GELCleaner', 'boostRAM', []);
+  },
+  clearTemp: function (success, error) {
+    exec(success, error, 'GELCleaner', 'clearTemp', []);
+  },
+  removeJunk: function (success, error) {
+    exec(success, error, 'GELCleaner', 'removeJunk', []);
+  },
+  optimizeBattery: function (success, error) {
+    exec(success, error, 'GELCleaner', 'optimizeBattery', []);
+  },
+  killBackground: function (success, error) {
+    exec(success, error, 'GELCleaner', 'killBackground', []);
+  }
+};
+
+module.exports = GELCleaner;

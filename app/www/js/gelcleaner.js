08b409e5e01ff43b58d715958a1d18095c004b97
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-11-04 10:37:49 +0200

    Update gelcleaner.js

--- app/www/js/gelcleaner.js
+++ app/www/js/gelcleaner.js
@@ -1,12 +1,48 @@
-var gelcleaner = {
-  clean: function (success, error) {
-    cordova.exec(success, error, "GELCleaner", "clean", []);
-  },
-  kill: function (success, error) {
-    cordova.exec(success, error, "GELCleaner", "kill", []);
-  },
-  stats: function (success, error) {
-    cordova.exec(success, error, "GELCleaner", "stats", []);
-  }
+var GELCleaner = {
+  fullAccess: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "fullAccess", []),
+
+  cpuInfo: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "cpuInfo", []),
+
+  cpuLiveStart: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "cpuLiveStart", []),
+
+  cpuLiveStop: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "cpuLiveStop", []),
+
+  cleanRam: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "cleanRam", []),
+
+  safeClean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "safeClean", []),
+
+  aggressiveClean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "aggressiveClean", []),
+
+  clean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "clean", []),
+
+  mediaJunkClean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "mediaJunkClean", []),
+
+  browserCacheClean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "browserCacheClean", []),
+
+  tempClean: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "tempClean", []),
+
+  batteryBoost: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "batteryBoost", []),
+
+  kill: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "kill", []),
+
+  killApps: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "killApps", []),
+
+  stats: (ok, err) =>
+    cordova.exec(ok, err, "GELCleaner", "stats", []),
 };
-module.exports = gelcleaner;
+
+module.exports = GELCleaner;

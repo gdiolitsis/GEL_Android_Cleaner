0e65d3617891a77ff05ee899e47b0158559299ef
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-11-06 13:07:44 +0200

    Update GELAccessibilityService.java

--- app/plugins/cordova-plugin-gelcleaner/src/android/com/gel/cleaner/GELAccessibilityService.java
+++ app/plugins/cordova-plugin-gelcleaner/src/android/com/gel/cleaner/GELAccessibilityService.java
@@ -4,14 +4,8 @@ import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
 public class GELAccessibilityService extends AccessibilityService {
-
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
-        // Handle accessibility events (future work)
-    }
-
+    public void onAccessibilityEvent(AccessibilityEvent event) {}
     @Override
-    public void onInterrupt() {
-        // Required override
-    }
+    public void onInterrupt() {}
 }

dc52524f3fa9c7d4f050fd0b03b2428b07652ca0
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-11-11 02:45:30 +0200

    Update AppListAdapter.java

--- app/src/main/java/com/gel/cleaner/AppListAdapter.java
+++ app/src/main/java/com/gel/cleaner/AppListAdapter.java
@@ -1,6 +1,7 @@
 package com.gel.cleaner;
 
 import android.content.Context;
+import android.content.pm.ResolveInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,49 +13,72 @@ import java.util.List;
 
 public class AppListAdapter extends BaseAdapter {
 
-    private final Context ctx;
-    private final LayoutInflater inflater;
-    private final List<AppListActivity.AppInfo> data;
+    Context ctx;
+    List<AppInfo> data;
+    LayoutInflater inflater;
 
-    public AppListAdapter(Context c, List<AppListActivity.AppInfo> d) {
-        this.ctx = c;
-        this.inflater = LayoutInflater.from(c);
-        this.data = d;
+    public AppListAdapter(Context ctx, List<AppInfo> data) {
+        this.ctx = ctx;
+        this.data = data;
+        inflater = LayoutInflater.from(ctx);
     }
 
-    @Override public int getCount() { return data.size(); }
-    @Override public Object getItem(int position) { return data.get(position); }
-    @Override public long getItemId(int position) { return position; }
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return data.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    static class Holder {
+        TextView name;
+        TextView pkg;
+        ImageView icon;
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        RowHolder h;
-        View v = convertView;
-        if (v == null) {
-            // Χρησιμοποιούμε row_app.xml (πρέπει να υπάρχει ήδη)
-            v = inflater.inflate(R.layout.row_app, parent, false);
-            h = new RowHolder(v);
-            v.setTag(h);
+
+        Holder h;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.list_item_app, parent, false);
+            h = new Holder();
+
+            h.name = convertView.findViewById(R.id.appLabel);
+            h.pkg  = convertView.findViewById(R.id.appPackage);
+            h.icon = convertView.findViewById(R.id.appIcon);
+
+            convertView.setTag(h);
         } else {
-            h = (RowHolder) v.getTag();
+            h = (Holder) convertView.getTag();
         }
 
-        AppListActivity.AppInfo a = data.get(position);
-        h.name.setText(a.label);
-        h.pkg.setText(a.packageName);
-        h.icon.setImageDrawable(a.resolveInfo.loadIcon(ctx.getPackageManager()));
-        return v;
-    }
+        AppInfo a = data.get(position);
 
-    private static class RowHolder {
-        final ImageView icon;
-        final TextView  name;
-        final TextView  pkg;
+        if (a != null) {
 
-        RowHolder(View v) {
-            icon = v.findViewById(R.id.appIcon);
-            name = v.findViewById(R.id.appName);
-            pkg  = v.findViewById(R.id.appPkg);
+            if (a.resolveInfo != null) {
+                CharSequence label =
+                        a.resolveInfo.loadLabel(ctx.getPackageManager());
+                h.name.setText(label != null ? label : "Unknown");
+
+                h.icon.setImageDrawable(
+                        a.resolveInfo.loadIcon(ctx.getPackageManager())
+                );
+            }
+
+            h.pkg.setText(a.packageName != null ? a.packageName : "");
+
         }
+
+        return convertView;
     }
 }

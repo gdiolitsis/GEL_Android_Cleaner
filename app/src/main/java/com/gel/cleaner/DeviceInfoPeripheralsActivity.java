7a8bcc920a25cd59701a7324c340043ced322de7
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-11-19 23:05:03 +0200

    Update DeviceInfoPeripheralsActivity.java

--- app/src/main/java/com/gel/cleaner/DeviceInfoPeripheralsActivity.java
+++ app/src/main/java/com/gel/cleaner/DeviceInfoPeripheralsActivity.java
@@ -45,6 +45,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeviceInfoPeripheralsActivity extends AppCompatActivity {
 

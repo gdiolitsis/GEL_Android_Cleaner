4cc7657a9e25fc4d0eadbeb0d2638d768e3ca947
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-11-23 20:05:18 +0200

    Update GELAutoActivityHook.java

--- app/src/main/java/com/gel/cleaner/GELAutoActivityHook.java
+++ app/src/main/java/com/gel/cleaner/GELAutoActivityHook.java
@@ -1,5 +1,6 @@
 package com.gel.cleaner;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +12,22 @@ public abstract class GELAutoActivityHook extends AppCompatActivity {
         GELAutoDP.init(this);
     }
 
-    public int dp(int x) {
-        return GELAutoDP.dp(x);
+    // Rotation / Fold-Unfold / Tablet resize / Locale change etc
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        GELAutoDP.init(this);
     }
 
-    public float sp(float x) {
-        return GELAutoDP.sp(x);
+    // Split-screen / freeform resize
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode) {
+        super.onMultiWindowModeChanged(isInMultiWindowMode);
+        GELAutoDP.init(this);
     }
+
+    // Handy wrappers so δεν αλλάζεις κώδικα
+    public int dp(int x) { return GELAutoDP.dp(x); }
+    public float sp(float x) { return GELAutoDP.sp(x); }
+    public int px(int x) { return GELAutoDP.px(x); }
 }

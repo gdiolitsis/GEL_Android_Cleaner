d3cf6b92bc3937068dc134c1b4b30e3ee0bd1af2
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2025-12-14 01:29:36 +0200

    Update DeviceInfoInternalActivity.java

--- app/src/main/java/com/gel/cleaner/DeviceInfoInternalActivity.java
+++ app/src/main/java/com/gel/cleaner/DeviceInfoInternalActivity.java
@@ -46,6 +46,8 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 public class DeviceInfoInternalActivity extends GELAutoActivityHook
         implements GELFoldableCallback {

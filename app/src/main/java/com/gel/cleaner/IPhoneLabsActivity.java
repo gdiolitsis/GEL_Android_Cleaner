a75d759497173c1a4afcc13ad863b64b02061b15
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-01-01 12:44:12 +0200

    Update IPhoneLabsActivity.java

--- app/src/main/java/com/gel/cleaner/IPhoneLabsActivity.java
+++ app/src/main/java/com/gel/cleaner/IPhoneLabsActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;

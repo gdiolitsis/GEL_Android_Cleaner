2638257ffe36b5a0c8a2ebb678767c691650a265
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-01-15 22:02:34 +0200

    Update MainActivity.java

--- app/src/main/java/com/gel/cleaner/MainActivity.java
+++ app/src/main/java/com/gel/cleaner/MainActivity.java
@@ -3,6 +3,8 @@
 
 package com.gel.cleaner;
 
+import com.gel.cleaner.iphone.AppleDeviceInfoInternalActivity;
+import com.gel.cleaner.iphone.AppleDeviceInfoPeripheralsActivity;
 import com.gel.cleaner.base.*;
 
 import android.content.Context;

f3fec89a4045a5722d8e2c712543834ad64cb048
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-01-19 16:05:08 +0200

    Update AppleDeviceInfoPeripheralsActivity.java

--- app/src/main/java/com/gel/cleaner/iphone/AppleDeviceInfoPeripheralsActivity.java
+++ app/src/main/java/com/gel/cleaner/iphone/AppleDeviceInfoPeripheralsActivity.java
@@ -7,7 +7,8 @@
 package com.gel.cleaner.iphone;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle
+import android.text.Html;   
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;

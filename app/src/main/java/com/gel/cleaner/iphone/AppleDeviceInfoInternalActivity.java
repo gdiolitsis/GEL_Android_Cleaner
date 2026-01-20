3478473567cd55fed5089fd14198bca9e86193d5
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-01-20 14:44:19 +0200

    Update AppleDeviceInfoInternalActivity.java

--- app/src/main/java/com/gel/cleaner/iphone/AppleDeviceInfoInternalActivity.java
+++ app/src/main/java/com/gel/cleaner/iphone/AppleDeviceInfoInternalActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.gel.cleaner.base.AppleSpecProvider;
 import com.gel.cleaner.R;
 
 public class AppleDeviceInfoInternalActivity extends Activity {

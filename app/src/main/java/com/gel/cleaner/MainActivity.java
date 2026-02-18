11d01a79a4a9725dc5adad484e7fc03b72183996
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-02-18 10:43:35 +0200

    Update MainActivity.java

--- app/src/main/java/com/gel/cleaner/MainActivity.java
+++ app/src/main/java/com/gel/cleaner/MainActivity.java
@@ -476,7 +476,7 @@ root.addView(cb);
         GradientDrawable skipBg = new GradientDrawable();
         skipBg.setColor(0xFF8B0000);
         skipBg.setCornerRadius(dp(10));
-        setStroke(dp(3), 0xFFFFD700);
+        skipBg.setStroke(dp(3), 0xFFFFD700);
         skipBtn.setBackground(skipBg);
         skipBtn.setLayoutParams(btnLp);
 

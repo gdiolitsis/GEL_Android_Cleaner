743bd48fea0b2a34b4b4594cf69fa947a5af8009
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-03-04 11:16:00 +0200

    Update DiagnosisMenuActivity.java

--- app/src/main/java/com/gel/cleaner/DiagnosisMenuActivity.java
+++ app/src/main/java/com/gel/cleaner/DiagnosisMenuActivity.java
@@ -19,6 +19,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.gel.cleaner.UIHelpers;
+
 public class DiagnosisMenuActivity extends GELAutoActivityHook
         implements GELFoldableCallback {
 

e1f288b0a4a02673c0ad2db63de25f81163e6537
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-03-06 12:17:22 +0200

    Update IPhoneLabsActivity.java

--- app/src/main/java/com/gel/cleaner/IPhoneLabsActivity.java
+++ app/src/main/java/com/gel/cleaner/IPhoneLabsActivity.java
@@ -16,6 +16,8 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
